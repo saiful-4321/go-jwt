ee07a9caefd1a6102a22e56a5aec2dd14acd59cf
saif-paywell <bd.saif321@gmail.com>
2023-02-08 00:23:03 +0600

    added JWT token creation method

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
 )
+
+var SECRET = []byte("super-secret-auth-key")
 
 func main() {
 	http.HandleFunc("/", Home)
@@ -12,4 +17,21 @@ func main() {
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Wellcome to home section")
+}
+
+func CreateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["exp"] = time.Now().Add(time.Hour).Unix()
+
+	tokenStr, err := token.SignedString(SECRET)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	return tokenStr, nil
 }
